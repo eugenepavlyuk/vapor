b8ec06c88425f55e7541a7eea8f302c5c04c6a74
Ievgen Pavliuk <ievgen.pavliuk@virtual-solution.com>
2018-08-19 15:30:48 +0200

    added unit test

--- FileReader/Sources/FileReader/FileReader.swift
+++ FileReader/Sources/FileReader/FileReader.swift
@@ -1,3 +1,15 @@
-struct FileReader {
-    var text = "Hello, World!"
+import Foundation
+
+class FileReader {
+	
+	class func readFile(_ fileName: String) -> String? {
+
+		let fileManager = FileManager.default
+
+		let url = URL(fileURLWithPath: fileManager.currentDirectoryPath)
+		let fullPath = url.appendingPathComponent(fileName)
+
+		return try? String(contentsOf: fullPath, encoding: .utf8)
+	}
+
 }
--- FileReader/Tests/FileReaderTests/FileReaderTests.swift
+++ FileReader/Tests/FileReaderTests/FileReaderTests.swift
@@ -2,15 +2,12 @@ import XCTest
 @testable import FileReader
 
 final class FileReaderTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(FileReader().text, "Hello, World!")
+    func testReadReturnsHelloWorld() {
+        XCTAssertEqual(FileReader.readFile("hello.txt"), "Hello, World")
     }
 
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testReadReturnsHelloWorld", testReadReturnsHelloWorld),
     ]
 }
